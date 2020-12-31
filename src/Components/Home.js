2255907d6d2f60e3f087b051aa180c92059777d4
Femi Akinyemi <santosfemakin@yahoo.com>
2020-12-31 19:34:53 +0100

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,249 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <header>
+        Eight Responsive Websites Challenge from{" "}
+        <a href="https://devchallenges.io/">Devchallenge.io</a>
+      </header>
+      <div className="content">
+        <div className="content__body">
+          <img
+            src="https://res.cloudinary.com/femakin/image/upload/v1608320195/404-DESTOP_ujvmmf.jpg"
+            alt="404page"
+            className="content__image"
+          />
+          <h2 className="content__tag">#REACT #CSS #responsive </h2>
+          <h2 className="content__title">404 ERROR PAGE</h2>
+          <p className="content__description">
+            In this project, I work with HTML, CSS and REACT to create a
+            responsive page. This page is similiar with a page. The design is
+            from devchallenge.io
+          </p>
+          <button className="content__demo">
+            {" "}
+            <a href="https://404-devchallenge.netlify.app/" className="btn1">
+              Demo
+            </a>
+          </button>
+          <button className="content__code">
+            <a href="https://github.com/femakin/404-page" className="btn2">
+              Code
+            </a>{" "}
+          </button>
+        </div>
+
+
+        <div className="content__body">
+          <img
+            src="https://res.cloudinary.com/femakin/image/upload/v1608660824/interior_one_begjsm.jpg"
+            alt="CONSULTANTPHOTO"
+            className="content__image"
+          />
+          <h2 className="content__tag">#REACT #CSS #responsive </h2>
+          <h2 className="content__title">CONSULTANT HOMEPAGE</h2>
+          <p className="content__description">
+            In this project, I work with HTML, CSS and REACT to create a
+            responsive page. This page is similiar with a page. The design is
+            from devchallenge.io
+          </p>
+          <button className="content__demo">
+            {" "}
+            <a href="https://consultantpage.netlify.app/" className="btn1">
+              Demo
+            </a>
+          </button>
+          <button className="content__code">
+            <a href="https://github.com/femakin/Interior-Consultant" className="btn2">
+              Code
+            </a>{" "}
+          </button>
+        </div>
+
+
+
+
+        <div className="content__body">
+        <img
+          src="https://res.cloudinary.com/femakin/image/upload/v1608398160/myteam-DESKTOP_qc5e9w.jpg"
+          alt="teamwebsite"
+          className="content__image"
+        />
+        <h2 className="content__tag">#REACT #CSS #responsive </h2>
+        <h2 className="content__title">MY TEAM WEBSITE</h2>
+        <p className="content__description">
+          In this project, I work with HTML, CSS and REACT to create a
+          responsive page. This page is similiar with a page. The design is
+          from devchallenge.io
+        </p>
+        <button className="content__demo">
+          {" "}
+          <a href="https://myteamdevchallenge.netlify.app/" className="btn1">
+            Demo
+          </a>
+        </button>
+        <button className="content__code">
+          <a href="https://github.com/femakin/My-team-page" className="btn2">
+            Code
+          </a>{" "}
+        </button>
+      </div>
+
+
+
+
+      <div className="content__body">
+        <img
+          src="https://res.cloudinary.com/femakin/image/upload/v1608888464/recipe2_qviucf.jpg"
+          alt="recipephoto"
+          className="content__image"
+        />
+        <h2 className="content__tag">#REACT #CSS #responsive </h2>
+        <h2 className="content__title">RECIPE BLOG</h2>
+        <p className="content__description">
+          In this project, I work with HTML, CSS and REACT to create a
+          responsive page. This page is similiar with a page. The design is
+          from devchallenge.io
+        </p>
+        <button className="content__demo">
+          {" "}
+          <a href="https://recipepaged.netlify.app/" className="btn1">
+            Demo
+          </a>
+        </button>
+        <button className="content__code">
+          <a href="https://github.com/femakin/Recipe-page" className="btn2">
+            Code
+          </a>{" "}
+        </button>
+      </div>
+
+
+      <div className="content__body">
+        <img
+          src="https://res.cloudinary.com/femakin/image/upload/v1609183292/checkout_1_nepdvh.jpg"
+          alt="checkoutphoto"
+          className="content__image"
+        />
+        <h2 className="content__tag">#REACT #CSS #responsive </h2>
+        <h2 className="content__title">CHECKOUT PAGE WEBSITE</h2>
+        <p className="content__description">
+          In this project, I work with HTML, CSS and REACT to create a
+          responsive page. This page is similiar with a page. The design is
+          from devchallenge.io
+        </p>
+        <button className="content__demo">
+          {" "}
+          <a href="https://checkoutpaged.netlify.app/" className="btn1">
+            Demo
+          </a>
+        </button>
+        <button className="content__code">
+          <a href="https://github.com/femakin/Checkout-Page" className="btn2">
+            Code
+          </a>{" "}
+        </button>
+      </div>
+
+
+
+
+
+      <div className="content__body">
+        <img
+          src="https://res.cloudinary.com/femakin/image/upload/v1608925799/gallery_one_udjrlt.jpg"
+          alt="galleryphoto"
+          className="content__image"
+        />
+        <h2 className="content__tag">#REACT #CSS #responsive </h2>
+        <h2 className="content__title">MY GALLERY WEBSITE</h2>
+        <p className="content__description">
+          In this project, I work with HTML, CSS and REACT to create a
+          responsive page. This page is similiar with a page. The design is
+          from devchallenge.io
+        </p>
+        <button className="content__demo">
+          {" "}
+          <a href="https://mygallerydev.netlify.app/" className="btn1">
+            Demo
+          </a>
+        </button>
+        <button className="content__code">
+          <a href="https://github.com/femakin/My-Gallery" className="btn2">
+            Code
+          </a>{" "}
+        </button>
+      </div>
+
+
+      <div className="content__body">
+        <img
+          src="https://res.cloudinary.com/femakin/image/upload/v1609335272/Edie2_zuuuco.jpg"
+          alt="ediephoto"
+          className="content__image"
+        />
+        <h2 className="content__tag">#REACT #CSS #responsive </h2>
+        <h2 className="content__title">EDIE HOMEPAGE</h2>
+        <p className="content__description">
+          In this project, I work with HTML, CSS and REACT to create a
+          responsive page. This page is similiar with a page. The design is
+          from devchallenge.io
+        </p>
+        <button className="content__demo">
+          {" "}
+          <a href="https://ediehome.netlify.app/" className="btn1">
+            Demo
+          </a>
+        </button>
+        <button className="content__code">
+          <a href="https://github.com/femakin/Edie-homepage" className="btn2">
+            Code
+          </a>{" "}
+        </button>
+      </div>
+
+
+
+      <div className="content__body">
+        <img
+          src="https://res.cloudinary.com/femakin/image/upload/v1609411491/portfolio1_pgfh1a.jpg"
+          alt="portfoliophoto"
+          className="content__image"
+        />
+        <h2 className="content__tag">#REACT #CSS #responsive </h2>
+        <h2 className="content__title">PORTFOLIO WEBSITE</h2>
+        <p className="content__description">
+          In this project, I work with HTML, CSS and REACT to create a
+          responsive page. This page is similiar with a page. The design is
+          from devchallenge.io
+        </p>
+        <button className="content__demo">
+          {" "}
+          <a href="https://%7Byour-demo-link.your-domain%7D/" className="btn1">
+            Demo
+          </a>
+        </button>
+        <button className="content__code">
+          <a href="https://github.com/femakin/PortfolioDevchallenge" className="btn2">
+            Code
+          </a>{" "}
+        </button>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    </div>
+  );
+}
